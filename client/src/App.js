cdff4e4e19eef7471c36256c1ea5c5ce4ce7d778
Komenpurity <jepkorirpurity8@gmail.com>
2023-07-03 19:12:11 +0300

    complete

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,12 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Home from './components/Home'
 
-import './App.css';
-
-function App() {
-/*   return (
-    <div className="App">
-       const [user, setUser] = useState(null);
-
-    useEffect(() => { 
-      fetch("/me").then((response) => {
-        if (response.ok) {
-          response.json().then((user) => setUser(user));
-        }
-      });
-    }, []);
-
-    if (user) {
-      return <h2>Welcome, {user.username}!</h2>;
-    } else {
-      return <Login onLogin={setUser} />;
-    }
-        </div>
-      ); */
+export default function App() {
+  return (
+    <div>
+      <Navbar />
+      <Home />
+    </div>
+  )
 }
-
-export default App;
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,15 +1,15 @@
 import React from 'react'
 
-export default function Navbar({ onLogout }) {
-    function handleLogout() {
-        fetch("/logout", {
-          method: "DELETE",
-        }).then(() => onLogout());
-      }
-    
-      return (
-        <header>
-          <button onClick={handleLogout}>Logout</button>
-        </header>
-      );
+export default function Navbar() {
+  return (
+  <nav className="navbar navbar-expand-lg navbar-light bg-light ">  
+    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div className="navbar-nav">
+        <a className="nav-item nav-link active" href="#">Home <span className="sr-only">(current)</span></a>
+        <a className="nav-item nav-link" href="#">Add to Cart</a>
+        <a className="nav-item nav-link" href="#">Contacts</a>
+      </div>
+    </div>
+  </nav>
+  )
 }
